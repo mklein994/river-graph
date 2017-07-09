9762bc49fca3877b2287b130c1ce650afac69e66
Matthew Klein <mklein994@gmail.com>
2017-07-08 19:16:08 -0500

    Read csv file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import * as d3 from 'd3';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  csvFiles: FileList;
+
+  parseRiverLevelsFile(files: File[]): void {
+    // console.log(csvFile);
+    const reader: FileReader = new FileReader();
+
+    reader.onload = () => {
+      this.processFile(reader.result);
+    }
+
+    if (files.length !== 0) {
+      reader.readAsText(files[0]);
+    }
+  }
+
+  processFile(data: any): void {
+    const riverLevels = d3.csvParse(data);
+    console.log(riverLevels.columns);
+  }
 }
